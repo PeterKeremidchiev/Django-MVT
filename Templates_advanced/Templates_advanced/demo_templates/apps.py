c39f5e5867f7204d90f9f3fb38fdadefc53f64f8
Peter Keremidchiev <spartak97@abv.bg>
2024-02-13 22:23:19 +0200

    Django templates advanced

---
+++ Templates_advanced/Templates_advanced/demo_templates/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DemoTemplatesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Templates_advanced.demo_templates'
