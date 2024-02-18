e654de2cc06e9b6e10f9ed0e8223a90a44d21e8e
Peter Keremidchiev <spartak97@abv.bg>
2024-02-18 22:37:25 +0200

    Added functionalities to the project

---
+++ PetstagramWorkshop101/PetstagramWorkshop101/photos/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from PetstagramWorkshop101.photos.models import Photo
+
+
+class PhotoCreateForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = '__all__'
+
+class PhotoEditForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        exclude = ['photo']
